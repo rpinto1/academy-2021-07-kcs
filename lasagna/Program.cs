9323a5f23691b83cf772922322bf228982be5f33
ruiccosta <rui.costa@kcsit.pt>
2021-08-03 14:52:36 +0000

    New project

---
+++ lasagna/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lasagna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
