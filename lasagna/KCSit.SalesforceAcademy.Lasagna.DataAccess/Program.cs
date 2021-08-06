086f9ea40a651bdeb48f2f475d6eaec6887ce322
ruiccosta <rui.costa@kcsit.pt>
2021-08-06 10:24:55 +0000

    Add DataAccess Project

---
+++ lasagna/KCSit.SalesforceAcademy.Lasagna.DataAccess/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KCSit.SalesforceAcademy.Lasagna.DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
