f15791b53c0e562d096e14c71a73d1afdd16e8b5
pedrofasilva@gmail.com <pedrofasilva@gmail.com>
2021-08-20 19:18:13 +0000

    add initial folder structure for JWT authentication

---
+++ lasagna/KCSit.SalesforceAcademy.Lasagna.Business/Interfaces/IUserService.cs
@@ -0,0 +1,12 @@
+﻿using KCSit.SalesforceAcademy.Lasagna.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KCSit.SalesforceAcademy.Lasagna.Business.Interfaces
+{
+    public interface IUserService
+    {
+        UserModel Authenticate(string username, string password);
+    }
+}
---
+++ lasagna/KCSit.SalesforceAcademy.Lasagna.Business/Models/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace KCSit.SalesforceAcademy.Lasagna.Business.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public Guid UserInfoId { get; set; }
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        public string EmailId { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        public string Token { get; set; }
+    }
+}
