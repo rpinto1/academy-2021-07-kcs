0b3eb0219f1dd5df5d107ad80d4fbeb6565b4460
pedrofasilva@gmail.com <pedrofasilva@gmail.com>
2021-08-23 15:27:29 +0000

    implement all data validation in UserModel class using Regex

--- lasagna/KCSit.SalesforceAcademy.Lasagna.Business/Models/UserModel.cs
+++ lasagna/KCSit.SalesforceAcademy.Lasagna.Business/Models/UserModel.cs
@@ -13,22 +13,26 @@ namespace KCSit.SalesforceAcademy.Lasagna.Business.Models
         [Required]
         [DataType(DataType.Text)]
         [StringLength(maximumLength: 15, MinimumLength = 2, ErrorMessage = "First Name must have 2 to 15 characters")]
+        [RegularExpression(@"^([a-zA-Z]+)$", ErrorMessage = "First Name has invalid characters")]
         public string FirstName { get; set; }
 
         [Required]
         [DataType(DataType.Text)]
         [StringLength(maximumLength: 15, MinimumLength = 2, ErrorMessage = "Last Name must have 2 to 15 characters")]
+        [RegularExpression(@"^([a-zA-Z]+)$", ErrorMessage = "Last Name has invalid characters")]
         public string LastName { get; set; }
 
         [Required]
         [Key]
         [DataType(DataType.EmailAddress)]
         [EmailAddress]
+        [RegularExpression(@"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", ErrorMessage = "Email Address is not valid")]
         public string EmailAdress { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
-        [StringLength(maximumLength: 100, MinimumLength = 8, ErrorMessage = "Password should have at least 8 characters")]
+        [RegularExpression(@"((?=.*\d)(?=.*[A-Z])(?=.*\W).{8,100})", 
+            ErrorMessage = "Password must have at least 8 characters length, at least one capital letter, at least one digit and at least one special character")]
         public string Password { get; set; }
 
         [Required]
