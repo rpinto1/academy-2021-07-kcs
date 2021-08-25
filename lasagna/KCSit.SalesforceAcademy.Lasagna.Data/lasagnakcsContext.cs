cfdb59f870f178cbd1b90bcacc0acdfc7b745a95
ruiccosta <rui.costa@kcsit.pt>
2021-08-25 09:32:21 +0000

    Add retry on timeout

--- lasagna/KCSit.SalesforceAcademy.Lasagna.Data/lasagnakcsContext.cs
+++ lasagna/KCSit.SalesforceAcademy.Lasagna.Data/lasagnakcsContext.cs
@@ -16,7 +16,8 @@ namespace KCSit.SalesforceAcademy.Lasagna.Data
         public lasagnakcsContext(DbContextOptions<lasagnakcsContext> options)
             : base(options)
         {
-            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = 180;
+            ((IObjectContextAdapter)this).ObjectContext.CommandTimeout = 1000;
+            
         }
 
         public virtual DbSet<BalanceSheet> BalanceSheets { get; set; }
@@ -41,7 +42,8 @@ namespace KCSit.SalesforceAcademy.Lasagna.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("server=tcp:academies-moongy.database.windows.net,1433;user=appadmin;password=qwert#4477;database=lasagna-kcs");
+                optionsBuilder.UseSqlServer("server=tcp:academies-moongy.database.windows.net,1433;user=appadmin;password=qwert#4477;database=lasagna-kcs",options =>options.EnableRetryOnFailure());
+                
             }
         }
 
