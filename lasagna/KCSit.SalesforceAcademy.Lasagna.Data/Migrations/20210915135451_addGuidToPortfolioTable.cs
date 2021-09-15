5eb9ad54e583336ee785c3658bf11db99f3e3f0c
pedrofasilva@gmail.com <pedrofasilva@gmail.com>
2021-09-15 14:02:11 +0000

    add webAppAdmin add Guid to Portfolio table

---
+++ lasagna/KCSit.SalesforceAcademy.Lasagna.Data/Migrations/20210915135451_addGuidToPortfolioTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace KCSit.SalesforceAcademy.Lasagna.Data.Migrations
+{
+    public partial class addGuidToPortfolioTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "Uuid",
+                table: "Portfolios",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Uuid",
+                table: "Portfolios");
+
+
+        }
+    }
+}
