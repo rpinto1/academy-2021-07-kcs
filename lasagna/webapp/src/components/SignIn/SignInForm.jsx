740c8a54b7876b313e52a5806bdaf1ffc5f602d1
VHCosta <vitor.costa@kcsit.pt>
2021-09-22 12:52:45 +0000

    Fix sign in operation

--- lasagna/webapp/src/components/SignIn/SignInForm.jsx
+++ lasagna/webapp/src/components/SignIn/SignInForm.jsx
@@ -7,8 +7,8 @@ import { Button, Checkbox, Container, Form } from 'semantic-ui-react';
 export default function SignInForm() {
 
     const [user, setUser] = useState({
-      EmailAddress: '',
-      Password: '', 
+        EmailAddress: '',
+        Password: '',
     });
 
     const [loggedUser, setLoggedUser] = useState({
@@ -16,101 +16,102 @@ export default function SignInForm() {
         token: ''
     });
 
-   //if all went well, the user is redirected to the user homepage
-   const [redirect, setRedirect] = useState(false);
-   //if user wishes to remain logged, token and id will be saved no localstorage, if not, on sessionstorage
-   const [keepMeLogged, setKeepMeLogged] = useState(false);
-  
+    //if all went well, the user is redirected to the user homepage
+    const [redirect, setRedirect] = useState(false);
+    //if user wishes to remain logged, token and id will be saved no localstorage, if not, on sessionstorage
+    const [keepMeLogged, setKeepMeLogged] = useState(false);
+
     const handleChange = (event) => {
-      const { id, value } = event.target
-      
-      setUser(prevState => ({
-          ...prevState,
-          [id]: value     
-      }));
-      };
-     
-  const handleKeepMeLogged = (event) => {
-      setKeepMeLogged(!keepMeLogged);
-  };
-    
-  const saveUser = () => {
-    console.log(loggedUser.id)
-    if(keepMeLogged) {
-      localStorage.setItem('id', loggedUser.id.toString());
-      localStorage.setItem('token', loggedUser.token.toString());
-      return;
-    } 
-      sessionStorage.setItem('id', loggedUser.id.toString());
-      sessionStorage.setItem('token', loggedUser.token.toString());
-  };
+        const { id, value } = event.target
 
-  const handleSubmit = () => {
-      fetch(`http://localhost:3010/api/SignIn`, {
-      method: 'POST',
-      headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(user)
-     }).then(res =>{
-       console.log(res)
-       console.log(res.headers.get('Set-Cookie'))
-      res.json()
-     } )
-     .then(data => {
-      setLoggedUser(data.result)
-      setRedirect(true)
-    })
-    .catch(error => console.log(error))
-    
-};  
+        setUser(prevState => ({
+            ...prevState,
+            [id]: value
+        }));
+    };
 
-    useEffect(() =>{
-      saveUser()
-    },[loggedUser]);
+    const handleKeepMeLogged = (event) => {
+        setKeepMeLogged(!keepMeLogged);
+    };
 
-console.log(user);
-console.log(loggedUser);
+    const saveUser = () => {
+        console.log(loggedUser.id)
+        if (keepMeLogged) {
+            localStorage.setItem('id', loggedUser.id.toString());
+            localStorage.setItem('token', loggedUser.token.toString());
+            return;
+        }
+        sessionStorage.setItem('id', loggedUser.id.toString());
+        sessionStorage.setItem('token', loggedUser.token.toString());
+    };
 
-      return (
-        <Container className= 'formulario'>
+    const handleSubmit = () => {
+        fetch(`http://localhost:3010/api/SignIn`, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }).then(res => {
+            console.log('test: ', res);
+            console.log(res.headers.get('Set-Cookie'));
+            return res.json();
+        })
+            .then(data => {
+                console.log('data ', data);
+                setLoggedUser(data.result);
+                setRedirect(true)
+            })
+            .catch(error => console.log(error))
+
+    };
+
+    useEffect(() => {
+        saveUser()
+    }, [loggedUser]);
+
+    console.log(user);
+    console.log(loggedUser);
+
+    return (
+        <Container className='formulario'>
             <h1>Sign in to your account</h1>
-            <Form onSubmit= {handleSubmit}>
-            <Form.Field> 
-                <label htmlFor="username">Username: </label>
-                <input
-                type="text"
-                value={user.EmailAddress}
-                placeholder="Enter your email address here"
-                id="EmailAddress"
-                onChange={handleChange}
-              />
-            </Form.Field>  
+            <Form onSubmit={handleSubmit}>
+                <Form.Field>
+                    <label htmlFor="username">Username: </label>
+                    <input
+                        type="text"
+                        value={user.EmailAddress}
+                        placeholder="Enter your email address here"
+                        id="EmailAddress"
+                        onChange={handleChange}
+                    />
+                </Form.Field>
 
-            <Form.Field> 
-                <label htmlFor="password">Password: </label>
-                <input
-                type="password"
-                value={user.Password}
-                placeholder="Enter your password here"
-                id="Password"
-                onChange={handleChange}
-                />
-            </Form.Field> 
-            <Form.Field>
-              <Link to ='/forgottenpassword'><p>I don't remember my password.</p></Link>
-            </Form.Field>
-            <Form.Field>
-              <Checkbox label='Keep me logged in!' onClick ={handleKeepMeLogged} />
-            </Form.Field>
-              <Button type="submit" id="submit_btn" >Sign in</Button>
+                <Form.Field>
+                    <label htmlFor="password">Password: </label>
+                    <input
+                        type="password"
+                        value={user.Password}
+                        placeholder="Enter your password here"
+                        id="Password"
+                        onChange={handleChange}
+                    />
+                </Form.Field>
+                <Form.Field>
+                    <Link to='/forgottenpassword'><p>I don't remember my password.</p></Link>
+                </Form.Field>
+                <Form.Field>
+                    <Checkbox label='Keep me logged in!' onClick={handleKeepMeLogged} />
+                </Form.Field>
+                <Button type="submit" id="submit_btn" >Sign in</Button>
             </Form>
-        
-        {/* { redirect &&
+
+            { redirect &&
             <Redirect to='/user/homepage' />
-        } */}
-        
+        }
+
         </Container>
 
 
